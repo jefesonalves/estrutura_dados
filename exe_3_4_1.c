0c7425810fda5e325bfeb98df6ed9d913ebc347e
jefesonalves <jefesonbezerra@gmail.com>
2023-07-30 22:13:58 -0300

    ajuste no array_length

--- exe_3_4_1.c
+++ exe_3_4_1.c
@@ -2,7 +2,7 @@
 
 int main() {
     int array_int[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
-    int array_length = sizeof(array_int) / sizeof(* array_int);
+    int array_length = sizeof(array_int) / sizeof(*array_int);
     for (int x = 0; x < array_length; x++) {
         printf("%d\n", array_int[x]);
     }
