9fbd910891bde79874aa97c0765dc786032b1641
jefesonalves <jefesonbezerra@gmail.com>
2023-08-27 18:06:42 -0300

    exercício 3.4.2

---
+++ exe_3_4_2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+char main() {
+    char array_int[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    int array_length = sizeof(array_int) / sizeof(*array_int);
+    for (int x = 0; x < array_length; x++) {
+        printf("%d\n", array_int[x]);
+    }
+
+    return 0;
+}
